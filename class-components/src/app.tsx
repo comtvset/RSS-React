fb74b753b689ee8108648c47322fa14f0304ebd8
comtvset <comtvset@gmail.com>
2024-07-03 20:23:47 +0200

    test11

--- class-components/src/app.tsx
+++ class-components/src/app.tsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import reactLogo from 'src/assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import 'src/App.css'
 
-function App() {
+export function App() {
   const [count, setCount] = useState(0)
 
   return (
--- class-components/src/main.tsx
+++ class-components/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from 'src/App.tsx';
+import { App } from 'src/App.tsx';
 import 'src/index.css';
 
 const rootElement = document.createElement('div');
