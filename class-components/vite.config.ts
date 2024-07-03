c23185aba769ebd5e4af77db794a566012bbdc89
comtvset <comtvset@gmail.com>
2024-07-03 19:07:32 +0200

    test2

--- class-components/vite.config.ts
+++ class-components/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  base: "class-components",
+  base: "class-componentsDDD",
   plugins: [tsconfigPaths(), react()],
 })
