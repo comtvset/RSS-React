695fa33ec222f86dec9070b761f625294ccac774
comtvset <comtvset@gmail.com>
2024-07-03 20:07:01 +0200

    test10

--- class-components/vite.config.ts
+++ class-components/vite.config.ts
@@ -9,7 +9,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // })
 
 export default defineConfig({
-  // base: './',
+  base: './',
   plugins: [tsconfigPaths(), react()],
   css: {
     modules: {
