6ccf5efdaf8b2f97a146b9fb3b7c5a97fc9e40bd
comtvset <comtvset@gmail.com>
2024-07-04 00:46:53 +0200

    test19

--- class-components/src/main.tsx
+++ class-components/src/main.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from 'src/App.tsx';
+// import App from 'src/App.tsx';
 import 'src/index.css';
+import AppNew from './AppNew';
 
 const rootElement = document.createElement('div');
 rootElement.id = 'root';
@@ -9,6 +10,7 @@ document.body.appendChild(rootElement);
 
 createRoot(rootElement).render(
     <React.StrictMode>
-        <App />
+        {/* <App /> */}
+        <AppNew />
     </React.StrictMode>
 );
