4c332d5986c75e5d659c25fc648dc860e769fb59
comtvset <comtvset@gmail.com>
2024-07-04 04:33:01 +0200

    test27

--- class-components/.eslintrc.cjs
+++ class-components/.eslintrc.cjs
@@ -2,20 +2,30 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: [
+    "react-refresh",
+    "react-compiler",
+    "prettier",
+    "@typescript-eslint",
+  ],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
+    "@typescript-eslint/no-explicit-any": "error",
+    "react-compiler/react-compiler": "error",
+    "no-console": "warn",
+    "prettier/prettier": "error",
   },
-}
+};
 // module.exports = {
 //   root: true,
 //   env: { browser: true, es2020: true },
--- class-components/src/main.tsx
+++ class-components/src/main.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from 'src/myApp.tsx';
-import 'src/index.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "src/myApp.tsx";
+import "src/index.css";
 
-const rootElement = document.createElement('div');
-rootElement.id = 'root';
+const rootElement = document.createElement("div");
+rootElement.id = "root";
 document.body.appendChild(rootElement);
 
 createRoot(rootElement).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
