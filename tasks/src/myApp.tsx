e833c40f6022ad5a9325eccf763fb2f825911302
comtvset <comtvset@gmail.com>
2024-07-04 20:50:09 +0200

    test 3

--- tasks/src/myApp.tsx
+++ tasks/src/myApp.tsx
@@ -5,7 +5,7 @@ export const App = () => {
     <>
       <h1>Get Start!</h1>
       <div>
-        <button onClick={() => {}}> Test Errorssss </button>
+        <button onClick={() => {}}> Test Error </button>
       </div>
     </>
   );
