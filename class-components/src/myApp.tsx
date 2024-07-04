1b393362383b3249201c85faa11c804ff3480402
comtvset <comtvset@gmail.com>
2024-07-04 12:35:38 +0200

    test29

--- class-components/src/myApp.tsx
+++ class-components/src/myApp.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import reactLogo from "src/assets/react.svg";
 import viteLogo from "/vite.svg";
-import "src/myApp.css";
+import "src/myApp.css"
 
 function App() {
   const [count, setCount] = useState(0)
