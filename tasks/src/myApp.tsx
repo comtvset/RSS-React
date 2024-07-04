8125cb4a6c5940211ac44b728e5eb202931543b9
comtvset <comtvset@gmail.com>
2024-07-04 19:20:41 +0200

    testing lint-staged test3

--- tasks/src/myApp.tsx
+++ tasks/src/myApp.tsx
@@ -3,7 +3,7 @@ import 'src/myApp.css';
 export const App = () => {
   return (
     <>
-    <h1>Get Start!</h1>
+      <h1>Get Start!</h1>
       <div>
         <button onClick={() => {}}> Test Error </button>
       </div>
