6074238ec51a6148322cd8263e6ee6048a62e999
Vadzim <49336945+comtvset@users.noreply.github.com>
2024-07-04 16:57:31 +0200

    init: add scss, preparing env for work

--- class-components/src/myApp.tsx
+++ class-components/src/myApp.tsx
@@ -1,26 +1,12 @@
-import { useState } from 'react';
-import reactLogo from 'src/assets/react.svg';
-import viteLogo from '/vite.svg';
 import 'src/myApp.css';
 
-function App() {
-  const [count, setCount] = useState(0);
-
+export const App = () => {
   return (
     <>
+    <h1>Get Start!</h1>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <button onClick={() => {}}> Test Error </button>
       </div>
     </>
   );
-}
-
-export default App;
+};
