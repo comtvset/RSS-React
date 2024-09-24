0b5a269dad31b2fc5ea6fc755879a8e57fe14baf
comtvset <comtvset@gmail.com>
2024-09-24 18:02:47 +0200

    fix: resolving problem with show details card

--- tasks/src/serveces/tools/text.tsx
+++ tasks/src/serveces/tools/text.tsx
@@ -1,10 +1,25 @@
 interface TextProps {
   label: string;
-  value: string;
+  value: string | string[];
 }
 
-export const Text: React.FC<TextProps> = ({ label, value }) => (
-  <p>
-    {label}: {value}
-  </p>
-);
+export const Text: React.FC<TextProps> = ({ label, value }) => {
+  const renderValue = () => {
+    if (Array.isArray(value)) {
+      return value.map((item, index) => (
+        <span key={index}>
+          {item}
+          <br />
+        </span>
+      ));
+    } else {
+      return String(value);
+    }
+  };
+
+  return (
+    <p>
+      {label}: {renderValue()}
+    </p>
+  );
+};
